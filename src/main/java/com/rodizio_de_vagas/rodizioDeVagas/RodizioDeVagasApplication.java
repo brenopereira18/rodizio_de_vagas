3a20d775ac1a60efe60ce15bf4bb7f6b3beadcc2
brenopereira18 <brenopereira@gmail.com>
2025-06-24 08:56:53 -0300

    feat: setup do projeto

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/RodizioDeVagasApplication.java
@@ -0,0 +1,13 @@
+package com.rodizio_de_vagas.rodizioDeVagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodizioDeVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodizioDeVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodizio_de_vagas/rodizioDeVagas/RodizioDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodizio_de_vagas.rodizioDeVagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodizioDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
