d26e4e415684a9bf9bf43e812797c548e73fbac8
brenopereira18 <brenopereira@gmail.com>
2025-06-24 10:44:01 -0300

    feat: cria entidade

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/entity/EnrollmentEntity.java
@@ -0,0 +1,32 @@
+package com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity;
+
+import com.rodizio_de_vagas.rodizioDeVagas.modules.user.entity.UserEntity;
+import com.rodizio_de_vagas.rodizioDeVagas.modules.work.entity.WorkEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+
+@Entity
+@Data
+@Table(name = "inscricao")
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnrollmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private WorkEntity workEntity;
+
+    private UserEntity userEntity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_da_inscricao")
+    private SubscriptionStatus subscriptionStatus = SubscriptionStatus.WAITING;
+
+    private Duration responseLimit = Duration.ofHours(2);
+}
