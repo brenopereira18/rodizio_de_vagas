f2f1027711199fa09e606ba0d79dfb2a11cc1881
brenopereira18 <brenopereira@gmail.com>
2025-06-27 08:35:23 -0300

    feat: cria métodos buscar serviços pelo status

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/work/repository/WorkRepository.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/work/repository/WorkRepository.java
@@ -1,11 +1,15 @@
 package com.rodizio_de_vagas.rodizioDeVagas.modules.work.repository;
 
 import com.rodizio_de_vagas.rodizioDeVagas.modules.work.entity.WorkEntity;
+import com.rodizio_de_vagas.rodizioDeVagas.modules.work.entity.WorkStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WorkRepository extends JpaRepository<WorkEntity, Long> {
 
     Optional<WorkEntity> findById(Long id);
+    List<WorkEntity> findByWorkStatus(WorkStatus status);
+    List<WorkEntity> findByWorkStatusIn(List<WorkStatus> status);
 }
