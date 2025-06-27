173623ee815aed6bc66468f5b80d43a10024cd89
brenopereira18 <brenopereira@gmail.com>
2025-06-27 08:37:44 -0300

    refactor: adiciona campo

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/user/entity/dto/RequestCreateUserDTO.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/user/entity/dto/RequestCreateUserDTO.java
@@ -1,7 +1,10 @@
 package com.rodizio_de_vagas.rodizioDeVagas.modules.user.entity.dto;
 
+import com.rodizio_de_vagas.rodizioDeVagas.modules.user.entity.UserRole;
+
 public record RequestCreateUserDTO(
     String fullName,
     String registration,
-    String phoneNumber
+    String phoneNumber,
+    UserRole userRole
 ) {}
