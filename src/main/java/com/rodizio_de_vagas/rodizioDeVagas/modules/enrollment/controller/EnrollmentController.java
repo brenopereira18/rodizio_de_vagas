a5ad1e1b39e3a098495dd0e21f2992d1adb0eb3c
brenopereira18 <brenopereira@gmail.com>
2025-07-01 08:08:06 -0300

    feat: cria endpoint que lista todas as inscrições com os respectivos fiscais e serviço

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/controller/EnrollmentController.java
@@ -2,11 +2,14 @@ package com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.controller;
 
 import com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity.EnrollmentEntity;
 import com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity.dto.RequestCreateEnrollmentDTO;
+import com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity.dto.ResponseWorkWithTaxDTO;
 import com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.service.EnrollmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/enrollment")
 @RestController
@@ -16,19 +19,20 @@ public class EnrollmentController {
     private EnrollmentService enrollmentService;
 
     @PostMapping
-    public ResponseEntity<String> createEnrollment(@RequestBody RequestCreateEnrollmentDTO dto) {
-        this.enrollmentService.createEnrollment(dto.workId(), dto.userId());
-        return ResponseEntity.status(HttpStatus.CREATED).body("Inscrição criada com sucesso");
+    public ResponseEntity<EnrollmentEntity> createEnrollment(@RequestBody RequestCreateEnrollmentDTO dto) {
+        EnrollmentEntity enrollment = this.enrollmentService.createEnrollment(dto.workId(), dto.userId());
+        return ResponseEntity.ok().body(enrollment);
     }
 
     @GetMapping
-    public ResponseEntity<EnrollmentEntity> getEnrollment() {
-
+    public ResponseEntity<List<ResponseWorkWithTaxDTO>> getEnrollmentsGroupedByWork(@RequestParam int month, @RequestParam int year) {
+        List<ResponseWorkWithTaxDTO> result = this.enrollmentService.getGroupedEnrollmentsByMonth(month, year);
+        return ResponseEntity.ok().body(result);
     }
 
     @PutMapping("/{id}/cancelled")
-    public ResponseEntity<String> cancelEnrollment(@PathVariable Long enrollmentId) {
-        this.enrollmentService.cancelEnrollment(enrollmentId);
+    public ResponseEntity<String> cancelEnrollment(@PathVariable Long id) {
+        this.enrollmentService.cancelEnrollment(id);
         return ResponseEntity.ok("Inscrição cancelada com sucesso. O serviço está disponivel novamente.");
     }
 }
