4644f7658e6c2bbd33625cec8df5a202a022a4a7
brenopereira18 <brenopereira@gmail.com>
2025-07-01 10:35:58 -0300

    refactor: adiciona novos campos

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/entity/dto/ResponseWorkWithTaxDTO.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/entity/dto/ResponseWorkWithTaxDTO.java
@@ -1,8 +1,16 @@
 package com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity.dto;
 
+import com.rodizio_de_vagas.rodizioDeVagas.modules.user.entity.UserEntity;
+import com.rodizio_de_vagas.rodizioDeVagas.modules.work.entity.Category;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record ResponseWorkWithTaxDTO(
     String workName,
+    String local,
+    LocalDateTime serviceDate,
+    UserEntity manager,
+    Category category,
     List<ResponseTaxInfosDTO> tax
 ) {}
