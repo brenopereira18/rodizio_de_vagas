c93efc505bd3ee64956014ba34e24dc84abad021
brenopereira18 <brenopereira@gmail.com>
2025-07-01 08:08:37 -0300

    feat: cria uma query para buscas os trabalhos do mês

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/enrollment/repository/EnrollmentRepository.java
@@ -4,11 +4,17 @@ import com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity.EnrollmentE
 import com.rodizio_de_vagas.rodizioDeVagas.modules.enrollment.entity.SubscriptionStatus;
 import com.rodizio_de_vagas.rodizioDeVagas.modules.work.entity.WorkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<EnrollmentEntity, Long> {
 
     int countByWorkEntityAndSubscriptionStatus(WorkEntity workEntity, SubscriptionStatus subscriptionStatus);
     Optional<EnrollmentEntity> findById(Long id);
+
+    @Query("SELECT e FROM EnrollmentEntity e WHERE e.workEntity.serviceDate BETWEEN :startDate AND :endDate")
+    List<EnrollmentEntity> findByWorkEntityServiceDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
