3d94dfd84abd056e33607344854c9ace72248b2b
brenopereira18 <brenopereira@gmail.com>
2025-07-02 17:00:58 -0300

    feat: cria consulta para verificar se um fiscal já foi notificado para trabalho determinado

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/notification/repository/NotificationRepository.java
@@ -0,0 +1,16 @@
+package com.rodizio_de_vagas.rodizioDeVagas.modules.notification.repository;
+
+import com.rodizio_de_vagas.rodizioDeVagas.modules.notification.entity.NotificationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+    Optional<NotificationEntity> findById(Long id);
+
+    @Query("SELECT n.userEntity.id FROM NotificationEntity n WHERE n.workEntity.id = :workId")
+    List<Long> findUserIdsByWorkId(@Param("workId") Long workId);
+}
