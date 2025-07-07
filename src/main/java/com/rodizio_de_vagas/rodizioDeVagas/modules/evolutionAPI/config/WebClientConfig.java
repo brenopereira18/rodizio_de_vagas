aa7405c10d20f9122f188335252e1856d6171f25
brenopereira18 <brenopereira@gmail.com>
2025-07-07 08:49:21 -0300

    feat: cria webClient

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/evolutionAPI/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.rodizio_de_vagas.rodizioDeVagas.modules.evolutionAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
