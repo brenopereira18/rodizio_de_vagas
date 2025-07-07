aabf53686b5df94fcc692d7e554b0080f694ffb8
brenopereira18 <brenopereira@gmail.com>
2025-07-07 08:54:52 -0300

    refactor: troca o nome dos campos

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/evolutionAPI/dto/EvolutionApiRequestDTO.java
@@ -0,0 +1,20 @@
+package com.rodizio_de_vagas.rodizioDeVagas.modules.evolutionAPI.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EvolutionApiRequestDTO {
+    private String number;
+    private String text;
+    private Integer delay = 10000;
+
+    public EvolutionApiRequestDTO(String number, String text) {
+        this.number = number;
+        this.text = text;
+        this.delay = 10000;
+    }
+}
