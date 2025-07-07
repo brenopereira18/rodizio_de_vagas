0bdc4404e5f1d0c127dac78f476e3802425ebf91
brenopereira18 <brenopereira@gmail.com>
2025-07-07 08:49:57 -0300

    feat: envia a notificação para o contato no whatsapp

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/evolutionAPI/service/WhatsappNotificationService.java
@@ -0,0 +1,36 @@
+package com.rodizio_de_vagas.rodizioDeVagas.modules.evolutionAPI.service;
+
+import com.rodizio_de_vagas.rodizioDeVagas.modules.evolutionAPI.dto.EvolutionApiRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+public class WhatsappNotificationService {
+
+    private final WebClient webClient;
+
+    @Value("${evolution.api.token}")
+    private String apiToken;
+
+    @Value("${evolution.api.url}")
+    private String apiUrl;
+
+    public void sendMessage(String number, String text) {
+        String formattedNumber = "55" + number;
+        EvolutionApiRequestDTO request = new EvolutionApiRequestDTO(formattedNumber, text);
+
+        webClient.post()
+            .uri(apiUrl)
+            .header("apikey", apiToken)
+            .header("Content-Type", "application/json")
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(String.class)
+            .doOnSuccess(response -> System.out.println("Mensagem enviada com sucesso: " + response))
+            .doOnError(error -> System.out.println("Erro ao enviar mensagem: " + error.getMessage()))
+            .subscribe(); // dispara a requisição de forma assíncrona
+    }
+}
