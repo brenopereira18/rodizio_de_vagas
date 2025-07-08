78959ff737addb4791a01b0a935faa2bab3cf248
brenopereira18 <brenopereira@gmail.com>
2025-07-08 10:07:32 -0300

    feat: adiciona autorização e pega o usuario que está autenticado

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/user/controller/UserController.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/modules/user/controller/UserController.java
@@ -8,8 +8,10 @@ import com.rodizio_de_vagas.rodizioDeVagas.modules.user.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -19,33 +21,38 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @PostMapping("/register")
     public ResponseEntity<UserEntity> createUser(@Valid @RequestBody RequestCreateUserDTO userDTO) {
         UserEntity user = this.userService.createUser(userDTO);
         return ResponseEntity.ok().body(user);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserEntity> getUser(@PathVariable Long id) {
-        UserEntity user = this.userService.getUser(id);
+    @GetMapping("/profile")
+    public ResponseEntity<UserEntity> getUser(Principal principal) {
+        String registration = principal.getName();
+        UserEntity user = this.userService.getUser(registration);
         return ResponseEntity.ok().body(user);
     }
 
-    @GetMapping
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @GetMapping("/managers")
     public ResponseEntity<List<ResponseManagerDTO>> getAllManagers() {
         List<ResponseManagerDTO> managers = this.userService.getAllManagers();
         return ResponseEntity.ok(managers);
     }
 
-    @PutMapping("/{id}/profile")
-    public ResponseEntity<UserEntity> updateUser(@PathVariable Long id,@Valid  @RequestBody RequestUpdateUserDTO dto) {
-        UserEntity user = this.userService.updateUser(id, dto);
+    @PutMapping("/profile")
+    public ResponseEntity<UserEntity> updateUser(Principal principal,@Valid  @RequestBody RequestUpdateUserDTO dto) {
+        String registration = principal.getName();
+        UserEntity user = this.userService.updateUser(registration, dto);
         return ResponseEntity.ok().body(user);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        this.userService.deleteUser(id);
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUser(@RequestParam String registration) {
+        this.userService.deleteUser(registration);
         return ResponseEntity.noContent().build();
     }
 }
