3d2f63c39184de3486708765388e0dedf33370aa
brenopereira18 <brenopereira@gmail.com>
2025-07-09 10:25:39 -0300

    feat: retorna página de login para os fiscais

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/web/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.rodizio_de_vagas.rodizioDeVagas.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/fiscal")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("error", "Matrícula ou senha incorreta.");
+        }
+        return "login";
+    }
+}
