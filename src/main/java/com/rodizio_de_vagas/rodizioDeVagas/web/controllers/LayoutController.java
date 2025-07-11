4f28095eae262276d42a68d54dd78d94836cc7aa
brenopereira18 <brenopereira@gmail.com>
2025-07-11 10:07:57 -0300

    feat: redireciona para a página inicial

---
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/web/controllers/LayoutController.java
@@ -0,0 +1,13 @@
+package com.rodizio_de_vagas.rodizioDeVagas.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LayoutController {
+
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/home/servicos";
+    }
+}
