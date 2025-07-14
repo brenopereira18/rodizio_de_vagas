1da81c639313c917b559592f086a44e178294473
brenopereira18 <brenopereira@gmail.com>
2025-07-14 11:08:04 -0300

    refactor: adiciona anotação em serviceDate

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/api/modules/work/entity/dto/RequestCreateOrUpdateWorkDTO.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/api/modules/work/entity/dto/RequestCreateOrUpdateWorkDTO.java
@@ -1,12 +1,16 @@
 package com.rodizio_de_vagas.rodizioDeVagas.api.modules.work.entity.dto;
 
 import com.rodizio_de_vagas.rodizioDeVagas.api.modules.work.entity.Category;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
-public record RequestCreateWorkDTO(
+public record RequestCreateOrUpdateWorkDTO(
+    Long id,
     String title,
     String location,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     LocalDateTime serviceDate,
     Long managerId,
     Category category,
