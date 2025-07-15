130616f6454c63f434ccb8aa5ba3de84df218f8b
brenopereira18 <brenopereira@gmail.com>
2025-07-15 16:26:42 -0300

    feat: adiciona novo campo

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/api/modules/user/entity/dto/RequestUpdateUserDTO.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/api/modules/user/entity/dto/RequestUpdateUserDTO.java
@@ -1,10 +1,14 @@
 package com.rodizio_de_vagas.rodizioDeVagas.api.modules.user.entity.dto;
 
+import com.rodizio_de_vagas.rodizioDeVagas.api.modules.work.entity.Category;
 import jakarta.validation.constraints.Size;
+
+import java.util.List;
 
 public record RequestUpdateUserDTO(
 
     @Size(min = 6, max = 12, message = "A senha deve ter de 6 a 12 caracteres")
     String password,
-    String phoneNumber
+    String phoneNumber,
+    List<Category> categorys
 ) {}
