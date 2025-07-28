8de8d82372828671fe4f9783ce1d9ceb7c8737c9
brenopereira18 <brenopereira@gmail.com>
2025-07-28 08:19:51 -0300

    feat: passa a lista de serviços do mês com as respectivas inscrições

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/web/controllers/RegistrationWebController.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/web/controllers/RegistrationWebController.java
@@ -1,16 +1,26 @@
 package com.rodizio_de_vagas.rodizioDeVagas.web.controllers;
 
+import com.rodizio_de_vagas.rodizioDeVagas.api.modules.enrollment.entity.dto.ResponseWorkWithTaxDTO;
+import com.rodizio_de_vagas.rodizioDeVagas.api.modules.enrollment.service.EnrollmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/home/inscricoes")
 public class RegistrationWebController {
 
+    @Autowired
+    private EnrollmentService enrollmentService;
+
     @GetMapping
     public String showRegistrations(Model model) {
+        List<ResponseWorkWithTaxDTO> servicesWithEnrollments = enrollmentService.getGroupedEnrollmentsByMonth();
+        model.addAttribute("servicos", servicesWithEnrollments);
         model.addAttribute("pageTitle", "Inscrições");
         return "fragments/enrollments";
     }
