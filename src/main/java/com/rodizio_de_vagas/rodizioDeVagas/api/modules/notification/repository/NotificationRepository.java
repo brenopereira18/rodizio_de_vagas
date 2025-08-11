b5ede3fa27728e410c6e39aa1de76a485322aa76
brenopereira18 <brenopereira@gmail.com>
2025-08-11 08:53:36 -0300

    feat: adiciona consulta para buscar por usuario

--- src/main/java/com/rodizio_de_vagas/rodizioDeVagas/api/modules/notification/repository/NotificationRepository.java
+++ src/main/java/com/rodizio_de_vagas/rodizioDeVagas/api/modules/notification/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.rodizio_de_vagas.rodizioDeVagas.api.modules.notification.repository;
 
 import com.rodizio_de_vagas.rodizioDeVagas.api.modules.notification.entity.NotificationEntity;
+import com.rodizio_de_vagas.rodizioDeVagas.api.modules.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,8 @@ import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
     Optional<NotificationEntity> findById(Long id);
+
+    List<NotificationEntity> findByUserEntity(UserEntity user);
 
     @Query("SELECT n.userEntity.id FROM NotificationEntity n WHERE n.workEntity.id = :workId")
     List<Long> findUserIdsByWorkId(@Param("workId") Long workId);
